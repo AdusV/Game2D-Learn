5b07e3270a6ed8bdf8f58dff094fb1d2fa9c4612
AdusV <adriankomendera@interia.pl>
2020-06-15 21:40:07 +0200

    Dodanie animacji i ruchu

---
+++ Game2d Learn/Assets/Scripts/HeroController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroController : MonoBehaviour
+{
+    Animator anim;
+    Rigidbody2D body;
+    public float jumpForce;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+        body = GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {//Lewy przycisk myszy 
+        if (Input.GetMouseButtonDown(0))
+        {
+            anim.SetTrigger("Jump");
+            //ForceMode2D.Impulse jednorazowe wystrzelenie w klatce
+            body.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+    }
+}
